698fac9fb363b3344ed6cdd112d5f2b24ffa8106
mskowron <michal.skowron92@gmail.com>
2025-10-14 23:35:54 +0200

    Spring boot app init

---
+++ src/main/java/com/skowron/Rekrutacja/RekrutacjaApplication.java
@@ -0,0 +1,13 @@
+package com.skowron.Rekrutacja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekrutacjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekrutacjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skowron/Rekrutacja/RekrutacjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skowron.Rekrutacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekrutacjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
