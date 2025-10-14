945f5eff3e1c598032f4db3fb8c44297480c0d56
mskowron <michal.skowron92@gmail.com>
2025-10-15 00:48:14 +0200

    PostsService injected and used

--- src/main/java/com/skowron/Rekrutacja/AppRunner.java
+++ src/main/java/com/skowron/Rekrutacja/AppRunner.java
@@ -1,15 +1,28 @@
 package com.skowron.Rekrutacja;
 
+import com.skowron.Rekrutacja.models.Post;
+import com.skowron.Rekrutacja.services.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 @Component
 public class AppRunner implements ApplicationRunner {
 
+    private final PostsService postsService;
+
+    @Autowired
+    public AppRunner(PostsService postsService) {
+        this.postsService = postsService;
+    }
+
     @Override
     public void run(ApplicationArguments args) {
-        System.out.println("Łololo");
+        List<Post> posts = postsService.getPosts();
+        System.out.println("Finished");
     }
 }
